e849313076916b75524e2ed31175a77943b835e1
ss <sashopro@gmail.com>
2012-05-28 09:04:16 +0300

    Added user following

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 class MicropostsController < ApplicationController
   before_filter :signed_in_user, only: [:create, :destroy]
-  before_filter :authorized_user, :only => :destroy
+  before_filter :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
@@ -20,7 +20,7 @@ class MicropostsController < ApplicationController
 
   private
 
-  def authorized_user
+  def correct_user
     @micropost = current_user.microposts.find_by_id(params[:id])
     redirect_to root_path if @micropost.nil?
   end
